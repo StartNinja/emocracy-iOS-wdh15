e4b9a59e1b2634af2f9f8e347a8e9070127da4c9
Stavros Filippidis <Stav@Stavross-MacBook-Pro.local>
2015-06-20 12:23:51 +0200

    Initial Commit

---
+++ Emocrcy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emocrcy
+//
+//  Created by Stavros Filippidis on 20.06.15.
+//  Copyright (c) 2015 Stavros Filippidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ EmocrcyTests/EmocrcyTests.swift
@@ -0,0 +1,36 @@
+//
+//  EmocrcyTests.swift
+//  EmocrcyTests
+//
+//  Created by Stavros Filippidis on 20.06.15.
+//  Copyright (c) 2015 Stavros Filippidis. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class EmocrcyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
